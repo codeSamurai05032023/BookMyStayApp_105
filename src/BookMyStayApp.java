5e306b27cc41b7d2ff65d03ab04210b54a8ea50c
Codesamurai05032023 <ankursinha2004@gmail.com>
2026-03-12 09:56:12 +0530

    UC7:Add-On Service Selection

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,30 @@
 import java.util.*;
 
 /**
+ * ABSTRACT CLASS - Room
+ * Represents the domain model for a hotel room.
+ */
+abstract class Room {
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.print("Beds: " + numberOfBeds + " | Size: " + squareFeet + " sqft | Price: " + pricePerNight);
+    }
+}
+
+class SingleRoom extends Room { public SingleRoom() { super(1, 250, 1500.0); } }
+class DoubleRoom extends Room { public DoubleRoom() { super(2, 400, 2500.0); } }
+class SuiteRoom extends Room { public SuiteRoom() { super(3, 750, 5000.0); } }
+
+/**
  * DOMAIN MODEL - AddOnService
  * Represents an optional service with a name and a fixed price.
  */
